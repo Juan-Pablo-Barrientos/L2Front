7350f851690eb0c330ff7e016433eb7f22dbaaa8
@Juan-Pablo-Barrientos <jpbarrientosros@gmail.com>
2022-09-12 20:16:57 -0300

    scroll fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -101,7 +101,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{ scrollOffset: [0, 0], scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
